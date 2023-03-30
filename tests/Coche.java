81e90d61b35eb1def33c19ff3b8ff5f233e5962f
Jesus <94246161+jbinade@users.noreply.github.com>
2023-03-30 21:47:17 +0200

    metodos refactorizados

--- tests/Coche.java
+++ tests/Coche.java
@@ -2,11 +2,11 @@ public class Coche {
 
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerar_Jesus(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerar_Jesus(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad < 0) velocidad = 0;
     }
